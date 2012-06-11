a1dd40b6dfc8371a1872331fe4de05ea3366a600
Michael Lam <treadsafely@gmail.com>
2012-06-11 12:59:27 +0100

    add ntwitter dependency

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require('express')
-,	port = process.env.PORT || 3000;
+,	port = process.env.PORT || 3000
+,	twitter = require('ntwitter');
 
 var app = express.createServer();
 
@@ -10,6 +11,18 @@ app.configure(function() {
 	app.use(express.static(__dirname + '/public'));
 	app.set('views', __dirname + '/views');
 	app.set('view engine', 'jade');
+});
+
+var twit = new twitter({
+	consumer_key: 'FaDwwAlb9Fmjc762nvrUA',
+	consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+	access_token_key: '605355787-7sGCezgRFDlaFsgGcMunDmae4blidlMA6GmCvIk9',
+	access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+});
+
+
+twit.verifyCredentials(function (err, data) {
+        console.log(data);
 });
 
 app.get('/', function(request, response){

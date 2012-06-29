efae0d8269399f1500a5bf9a014d12f7059e9364
Michael Lam <treadsafely@gmail.com>
2012-06-29 09:28:07 +0100

    minor css styling to nav

--- public/js/main.js
+++ public/js/main.js
@@ -1,24 +1,24 @@
 $(document).ready(function(){
 	console.log(window.location);
 
-	switch(window.location.pathname){
-		case '/':
-			$("#blue-burst").addClass("blue-burst-home");
-			break;
-		case '/our-story':
-			$("#blue-burst").addClass("blue-burst-our-story");
-			break;
-		case '/flavours':
-			$("#blue-burst").addClass("blue-burst-flavours");
-			break;
-		case '/hire-us':
-			$("#blue-burst").addClass("blue-burst-hire-us");
-			break;
-		case '/sponsors':
-			$("#blue-burst").addClass("blue-burst-sponsors");
-			break;
-		default:
-			break;
-	}
+	// switch(window.location.pathname){
+	// 	case '/':
+	// 		$("#blue-burst").addClass("blue-burst-home");
+	// 		break;
+	// 	case '/our-story':
+	// 		$("#blue-burst").addClass("blue-burst-our-story");
+	// 		break;
+	// 	case '/flavours':
+	// 		$("#blue-burst").addClass("blue-burst-flavours");
+	// 		break;
+	// 	case '/hire-us':
+	// 		$("#blue-burst").addClass("blue-burst-hire-us");
+	// 		break;
+	// 	case '/sponsors':
+	// 		$("#blue-burst").addClass("blue-burst-sponsors");
+	// 		break;
+	// 	default:
+	// 		break;
+	// }
 	
 });

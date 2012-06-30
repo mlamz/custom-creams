cb2ee234d64a9c07eb09b20f8ccf9b1ded4da227
Michael Lam <michael@michaellam.co.uk>
2012-06-30 18:52:36 +0100

    changed top nav to text

--- public/js/main.js
+++ public/js/main.js
@@ -1,24 +1,33 @@
 $(document).ready(function(){
 	console.log(window.location);
 
-	// switch(window.location.pathname){
-	// 	case '/':
-	// 		$("#blue-burst").addClass("blue-burst-home");
-	// 		break;
-	// 	case '/our-story':
-	// 		$("#blue-burst").addClass("blue-burst-our-story");
-	// 		break;
-	// 	case '/flavours':
-	// 		$("#blue-burst").addClass("blue-burst-flavours");
-	// 		break;
-	// 	case '/hire-us':
-	// 		$("#blue-burst").addClass("blue-burst-hire-us");
-	// 		break;
-	// 	case '/sponsors':
-	// 		$("#blue-burst").addClass("blue-burst-sponsors");
-	// 		break;
-	// 	default:
-	// 		break;
-	// }
+	switch(window.location.pathname){
+		case '/':
+			//$("#blue-burst").addClass("blue-burst-home");
+			$("ul.nav-buttons li a:eq(0)").addClass("highlight");
+			break;
+		case '/our-story':
+			$("ul.nav-buttons li a:eq(1)").addClass("highlight");
+
+			//$("#blue-burst").addClass("blue-burst-our-story");
+			break;
+		case '/flavours':
+			$("ul.nav-buttons li a:eq(2)").addClass("highlight");
+
+			//$("#blue-burst").addClass("blue-burst-flavours");
+			break;
+		case '/hire-us':
+			$("ul.nav-buttons li a:eq(3)").addClass("highlight");
+
+			//$("#blue-burst").addClass("blue-burst-hire-us");
+			break;
+		case '/sponsors':
+			$("ul.nav-buttons li a:eq(4)").addClass("highlight");
+
+			//$("#blue-burst").addClass("blue-burst-sponsors");
+			break;
+		default:
+			break;
+	}
 	
 });

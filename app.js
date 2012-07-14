75b4c29e2342cb646a1a629b6fa1a07d9fa27056
Michael Lam <michael@michaellam.co.uk>
2012-07-14 08:19:33 +0100

    added home and hire-us images. changed typography to be more consistent throughout

--- app.js
+++ app.js
@@ -21,11 +21,13 @@ app.configure(function() {
 
 
 parser.on('article', function (article){
-	var date = moment(JSON.stringify(article.pubDate), "YYYY-MM-DDTHH:mm.ss.SSSZ");
+	if (homeViewModel.twitterFeed.length < 8){
+		var date = moment(JSON.stringify(article.pubDate), "YYYY-MM-DDTHH:mm.ss.SSSZ");
 
-	homeViewModel.twitterFeed.push([date.format("dddd, MMMM Do YYYY"),
-		JSON.stringify(article.title.replace("Custom_Creams: ",""))]);
-    });
+		homeViewModel.twitterFeed.push([date.format("dddd, MMMM Do YYYY"),
+			JSON.stringify(article.title.replace("Custom_Creams: ",""))]);
+	}
+});
 
 
 app.get('/', function(req, res){
